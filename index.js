ebcca6dfa3ed6586a0f0021d2e0d2c672fab982f
Dustin <dustinhollowaypdx@gmail.com>
2023-03-22 08:25:49 -0700

    commit find method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(array) {
+  let thing = array.find((el) => {
+    return el.result === "W";
+  });
+  if (thing) {
+    return thing.year;
+  } else {
+    return undefined;
+  }
+}
+superbowlWin(record);
